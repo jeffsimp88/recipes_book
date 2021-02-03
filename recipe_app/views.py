6cdae4df7103a54db9f12aaa54c6e1575e5201e2
jeffsimp88 <jdsimpson806@gmail.com>
2021-02-03 14:10:48 -0500

    Created functions for recipe /author detail pages

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -4,4 +4,15 @@ from recipe_app.models import Author, Recipe
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    recipes = Recipe.objects.all()
+    return render(request, "index.html", {
+        "recipes": recipes
+    })
+
+def recipe_details(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    return render(request, "recipe_detail.html", {"recipe": recipe})
+
+def author_details(request, author_id):
+    author = Author.objects.get(id=author_id)
+    return render(request, "author_detail.html", {"author": author})
