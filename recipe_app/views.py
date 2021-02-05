08871a429a20c60c91dd201146ebe5cac3e0343f
jeffsimp88 <jdsimpson806@gmail.com>
2021-02-05 13:27:44 -0500

    Added renders of author & recipe forms

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 
 from recipe_app.models import Author, Recipe
+from recipe_app.forms import AddRecipeForm, AddAuthorForm
 
 # Create your views here.
 def index(request):
@@ -17,3 +18,33 @@ def author_details(request, author_id):
     author = Author.objects.get(id=author_id)
     recipes = Recipe.objects.filter(author=author)
     return render(request, "author_detail.html", {"author": author, "recipes": recipes})
+
+def add_recipe(request):
+    context = {'heading': "Add a Recipe"}
+    if request.method == "POST":
+        form = AddRecipeForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            new_item = Recipe.objects.create(
+                title=data['title'],
+                author = data['author'],
+                description = data['description'],
+                time_required = data['time_required'],
+                instructions = data['instructions'],
+
+            )
+            return HttpResponseRedirect('/')
+            # return HttpResponseRedirect(reverse('recipe_details', args=[new_item.id]) )
+            
+
+    form = AddRecipeForm()
+    context.update({'form': form})
+    return render(request, "generic_form.html", context)
+
+def add_author(request):
+    if request.method == 'POST':
+        form=AddAuthorForm(request.POST)
+        form.save()
+        return HttpResponseRedirect('/')
+    form = AddAuthorForm()
+    return render(request, "generic_form.html", {'form': form, 'heading': "Add an Author"})
