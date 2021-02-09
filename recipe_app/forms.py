6fccde246234f4f3713c57a7d67825dead896ae2
jeffsimp88 <jdsimpson806@gmail.com>
2021-02-09 14:19:13 -0500

    Added forms for signing up and login

--- recipe_app/forms.py
+++ recipe_app/forms.py
@@ -11,8 +11,17 @@ class AddAuthorForm(forms.ModelForm):
 
 class AddRecipeForm(forms.Form):
     title = forms.CharField(max_length=150)
-    author = forms.ModelChoiceField(queryset=Author.objects.all())
+    # author = forms.ModelChoiceField(queryset=Author.objects.all())
     description = forms.CharField(widget=forms.Textarea)
     time_required = forms.CharField(max_length=150)
     instructions = forms.CharField(widget=forms.Textarea)
 
+class SignupForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    bio = forms.CharField(max_length=150)
+    username = forms.CharField(max_length=36)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=36)
+    password = forms.CharField(widget=forms.PasswordInput)
